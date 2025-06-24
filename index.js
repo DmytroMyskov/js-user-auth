3fff8e4995b1e7ad2f6b518c2064489cd243eb0a
Dmytro <coulheyman@gmail.com>
2025-06-24 21:16:31 +0300

    add CRUD functionality for notes and update user form handling

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const users = [
   { id: nextId++, login: 'Alice', password: '5678' },
   { id: nextId++, login: 'Charlie', password: 'abcd' },
 ]
-global.users = users
+const notes = [
+  {
+    id: nextId++,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa natus dignissimos vel maxime nobis esse quas nesciunt officia assumenda architecto atque, eveniet magni soluta explicabo harum consequuntur exercitationem possimus aliquid perspiciatis. Aperiam, minus doloremque? Commodi ex delectus quidem excepturi ducimus. Facilis nulla sint laudantium a laborum illo eius voluptas? Quis inventore voluptas earum temporibus eum ad tempore impedit laborum, quod reprehenderit, facere itaque, asperiores eius ducimus molestias obcaecati ex magni? Harum quam ab nemo similique deleniti! Mollitia, deserunt. Voluptatibus, vero natus itaque veritatis aperiam similique quas alias inventore, doloremque porro incidunt adipisci fugiat aspernatur eaque esse nihil placeat dicta fuga corrupti nostrum amet? Commodi laudantium non eligendi blanditiis quasi fugit labore omnis cum ea fugiat numquam ipsum voluptates suscipit modi nulla nisi, at itaque perferendis, voluptatum reiciendis eius beatae quis? Non voluptatibus corrupti dignissimos ad adipisci repellendus earum aspernatur at placeat optio in, totam asperiores, sequi quis veritatis suscipit quibusdam iure. Dolorem aspernatur iure in mollitia nihil, nemo eius! Quae odit quos beatae hic veniam minus eligendi quia voluptas earum, quis autem doloribus, doloremque in assumenda eum. Iusto aliquam facere ea earum architecto harum ad accusantium at nemo atque sapiente maiores, maxime magnam aliquid? Ducimus exercitationem laboriosam repellat unde quasi amet numquam, magnam adipisci ea enim, itaque harum reiciendis laborum nisi laudantium dolores, eaque qui. Odio nulla ducimus architecto excepturi necessitatibus dolore exercitationem veritatis molestiae explicabo sit minus fugit eaque quod, dolores, aperiam sint dignissimos dolor. Qui, ipsam quas omnis, unde expedita, doloribus ab eligendi atque iste sint numquam! Aut ea magni ipsam earum officia mollitia quam tempora, voluptatibus suscipit esse et eum nesciunt sequi quibusdam necessitatibus id ducimus saepe nostrum dolores. Qui obcaecati in explicabo, architecto repellat omnis cumque magni! Non cupiditate, magni a sint consequuntur quidem molestias ab minus voluptatum nesciunt explicabo atque fugit impedit rem, quas debitis.",
+  },
+  { id: nextId++, text: "I'm Bob, I love to code and teach others how to do it. I'm a full-stack developer with a passion for the web." },
+  { id: nextId++, text: "I'm Alice, I'm a designer and I love to create beautiful and functional interfaces. I'm also a full-stack developer and I love to learn new things." },
+]
 const types = {
   html: 'text/html; charset=utf-8',
   css: 'text/css; charset=utf-8',
@@ -61,6 +68,10 @@ async function handleApi(request, response) {
       response.end(JSON.stringify(users))
       break
 
+    case 'GET:notes':
+      response.end(JSON.stringify(notes))
+      break
+
     case 'POST:user':
       const { login, password } = await getBody(request)
 
@@ -68,10 +79,25 @@ async function handleApi(request, response) {
       response.end(JSON.stringify({ success: true }))
       break
 
-    case 'DELETE:user':
+    case 'POST:note':
+      const { text } = await getBody(request)
+
+      addNote(text)
+      response.end(JSON.stringify({ success: true }))
+      break
+
+    case 'DELETE:user': {
       const { id } = await getBody(request)
 
       deleteUser(id)
+      response.end(JSON.stringify({ success: true }))
+      break
+    }
+
+    case 'DELETE:note':
+      const { id } = await getBody(request)
+
+      deleteNote(id)
       response.end(JSON.stringify({ success: true }))
       break
 
@@ -94,8 +120,19 @@ function addUser(login, password) {
   users.push(user)
 }
 
+function addNote(text) {
+  const note = { id: nextId++, text }
+  notes.push(note)
+}
+
 function deleteUser(id) {
   const i = users.findIndex(user => user.id === id)
   if (i === -1) return
   users.splice(i, 1)
+}
+
+function deleteNote(id) {
+  const i = notes.findIndex(note => note.id === id)
+  if (i === -1) return
+  notes.splice(i, 1)
 }
--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-const form = document.getElementById('new-user')
+const newUserForm = document.getElementById('new-user')
 const userList = document.getElementById('users')
 
-let users = await getUsers()
+const newNoteForm = document.getElementById('new-note')
+const noteList = document.getElementById('notes')
 
-form.onsubmit = handleSubmit
-userList.onclick = handleClick
+let users = await getUsers()
+let notes = await getNotes()
+
+newUserForm.onsubmit = handleNewUserSubmit
+userList.onclick = handleUserListClick
+
+newNoteForm.onsubmit = handleNewNoteSubmit
+noteList.onclick = handleNoteListClick
 
 showUsers()
+showNotes()
 
-async function handleSubmit(e) {
+async function handleNewUserSubmit(e) {
   e.preventDefault()
 
-  const login = form.login.value
-  const password = form.password.value
+  const login = newUserForm.login.value
+  const password = newUserForm.password.value
 
   await addUser(login, password)
 
-  form.reset()
+  newUserForm.reset()
   users = await getUsers()
   showUsers()
 }
 
-async function handleClick(e) {
+async function handleNewNoteSubmit(e) {
+  e.preventDefault()
+
+  const text = newNoteForm.text.value
+
+  await addNote(text)
+
+  newNoteForm.reset()
+  notes = await getNotes()
+  showNotes()
+}
+
+async function handleUserListClick(e) {
   if (e.target.tagName != "BUTTON") return
 
   const btn = e.target
@@ -32,6 +52,19 @@ async function handleClick(e) {
 
   users = await getUsers()
   showUsers()
+}
+
+async function handleNoteListClick(e) {
+  if (e.target.tagName != "BUTTON") return
+
+  const btn = e.target
+  const item = btn.closest('li')
+  const { id } = item.dataset
+
+  await deleteNote(+id)
+
+  notes = await getNotes()
+  showNotes()
 }
 
 async function addUser(login, password) {
@@ -45,6 +78,17 @@ async function addUser(login, password) {
   return fetch('/api/user', init)
 }
 
+async function addNote(text) {
+  const data = { text }
+  const init = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    body: JSON.stringify(data),
+  }
+
+  return fetch('/api/note', init)
+}
+
 async function deleteUser(id) {
   const data = { id }
   const init = {
@@ -56,9 +100,25 @@ async function deleteUser(id) {
   return fetch('/api/user', init)
 }
 
+async function deleteNote(id) {
+  const data = { id }
+  const init = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    body: JSON.stringify(data),
+  }
+
+  return fetch('/api/note', init)
+}
+
 async function getUsers() {
-  const respone = await fetch('/api/users')
-  return respone.json()
+  const response = await fetch('/api/users')
+  return response.json()
+}
+
+async function getNotes() {
+  const response = await fetch('/api/notes')
+  return response.json()
 }
 
 function showUsers() {
@@ -71,11 +131,36 @@ function showUsers() {
   userList.replaceChildren(...items)
 }
 
+function showNotes() {
+  const items = []
+
+  for (const note of notes) {
+    items.push(buildNoteItem(note))
+  }
+
+  noteList.replaceChildren(...items)
+}
+
 function buildUserItem(user) {
   const li = document.createElement('li')
   const btn = document.createElement('button')
   li.dataset.id = user.id
   btn.innerHTML = '&times;'
   li.append(user.login, ' ', btn)
+  return li
+}
+
+function buildNoteItem(note) {
+  const li = document.createElement('li')
+  const details = document.createElement('details')
+  const summary = document.createElement('summary')
+  const p = document.createElement('p')
+  const btn = document.createElement('button')
+  li.dataset.id = note.id
+  btn.innerHTML = '&times;'
+  li.append(details, btn)
+  details.append(summary, p)
+  summary.append(note.text.slice(0, 80))
+  p.append(note.text)
   return li
 }
